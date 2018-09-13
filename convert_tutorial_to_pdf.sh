050dbf78009d3f0655decc774b40fd2e2f6f64bf
wxgopher <wxguojlu@gmail.com>
2018-09-13 11:31:02 -0600

    elaborating on sh

--- convert_tutorial_to_pdf.sh
+++ convert_tutorial_to_pdf.sh
@@ -1,3 +1,6 @@
+# Prerequisite: pandoc/tex distribution
+# Available options can be found in https://pandoc.org/MANUAL.html#variables-for-latex and eisvogel.latex
+
 mkdir -p ~/.pandoc/templates
 cp eisvogel.latex ~/.pandoc/templates
 pandoc CHM-tutorial.md -o CHM-tutorial.pdf --from markdown --template eisvogel --listings -V toc -V titlepage=true -V toc-own-page -V book -V title="CHM Tutorial"
